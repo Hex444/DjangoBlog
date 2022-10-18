cf0c6fabb441641f42fa2cc66711a3971f572aa4
realhex444 <ayaansanjeev@gmail.com>
2022-10-18 16:04:35 +0530

    added login logout sys

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -6,8 +6,13 @@ the include func chops the already rendered part of the url in this
 case http://localhost:8000/ is already rendered so that part is removed
 and an empty string is sent to blog.urls file
 '''
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('register/',user_views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(req):
+    if req.method=='POST':
+        form=UserRegistrationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(req,f'Acount created for {username}! You can now log in')
+            return redirect('login')
+    else:
+        form=UserRegistrationForm()
+    return render(req,'users/register.html',{'form':form})
+
+'''
+messages.debug
+messages.info
+messages.success
+warning
+error
+'''
